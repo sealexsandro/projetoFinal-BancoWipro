2d1e7eb9883d39cb666d93eb8cc3c3af8eeb9551
dougdasilva <douglas_wfs@hotmail.com>
2022-04-27 17:31:59 -0300

    managertest

---
+++ src/test/java/com/wipro/projetofinal/ManagerServiceTest.java
@@ -0,0 +1,14 @@
+package com.wipro.projetofinal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ManagerServiceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
